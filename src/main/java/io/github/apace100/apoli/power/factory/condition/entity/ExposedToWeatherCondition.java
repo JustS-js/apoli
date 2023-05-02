f8e353516211d26ab5ad2dadcc64f7357fb43ca1
eggohito <66972816+eggohito@users.noreply.github.com>
2023-05-02 22:30:21 +0800

    Added `exposed_to_weather` entity condition type

---
+++ src/main/java/io/github/apace100/apoli/power/factory/condition/entity/ExposedToWeatherCondition.java
@@ -0,0 +1,54 @@
+package io.github.apace100.apoli.power.factory.condition.entity;
+
+import io.github.apace100.apoli.Apoli;
+import io.github.apace100.apoli.power.factory.condition.ConditionFactory;
+import io.github.apace100.calio.data.SerializableData;
+import io.github.apace100.calio.data.SerializableDataType;
+import io.github.apace100.calio.data.SerializableDataTypes;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+
+public class ExposedToWeatherCondition {
+
+    public static boolean condition(SerializableData.Instance data, Entity entity) {
+
+        Biome.Precipitation precipitation = data.get("weather");
+        Boolean thundering = data.get("thundering");
+
+        return exposedToWeather(entity, precipitation)
+            || (thundering == null || (thundering && entity.world.isThundering()));
+
+    }
+
+    private static boolean exposedToWeather(Entity entity, Biome.Precipitation precipitation) {
+
+        BlockPos downBlockPos = entity.getBlockPos();
+        BlockPos upBlockPos = new BlockPos(downBlockPos.getX(), entity.getBoundingBox().maxY, downBlockPos.getZ());
+
+        return exposedToWeather(entity.world, downBlockPos, precipitation)
+            || exposedToWeather(entity.world, upBlockPos, precipitation);
+
+    }
+
+    private static boolean exposedToWeather(World world, BlockPos blockPos, Biome.Precipitation precipitation) {
+        Biome biome = world.getBiome(blockPos).value();
+        return world.isRaining()
+            && world.isSkyVisible(blockPos)
+            && world.getTopPosition(Heightmap.Type.MOTION_BLOCKING, blockPos).getY() < blockPos.getY()
+            && (precipitation == Biome.Precipitation.SNOW ? biome.isCold(blockPos) : (precipitation == null || biome.getPrecipitation() == precipitation));
+    }
+
+    public static ConditionFactory<Entity> getFactory() {
+        return new ConditionFactory<>(
+            Apoli.identifier("exposed_to_weather"),
+            new SerializableData()
+                .add("weather", SerializableDataType.enumValue(Biome.Precipitation.class), null)
+                .add("thundering", SerializableDataTypes.BOOLEAN, null),
+            ExposedToWeatherCondition::condition
+        );
+    }
+
+}
